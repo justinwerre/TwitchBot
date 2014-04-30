11553400fa0a6fb1ade9020ac1cd53fb8d6e9e8e
justinwerre <werrejustin@gmail.com>
2014-04-30 13:54:50 -0600

    setting parsing tested and finished

--- FileHandlerTestFixture.py
+++ FileHandlerTestFixture.py
@@ -4,17 +4,25 @@ import FileHandler
 class TestFileHandler(unittest.TestCase):
 	def setUp(self):
 		self.fh = FileHandler.FileHandler()
+		self.options = self.fh.getOptions("testconfig.ini")
 
 	def tearDown(self):
 		del self.fh
 
 	def testParseUsername(self):
-		options = self.fh.getOptions("testconfig.ini")
-		self.assertEqual(options['nickname'], "username")
+		self.assertEqual(self.options['nickname'], "username")
 
 	def testParseOauth(self):
-		options = self.fh.getOptions("testconfig.ini")
-		self.assertEqual(options['oauth'], "token")
+		self.assertEqual(self.options['oauth'], "token")
+
+	def testParseChannel(self):
+		self.assertEqual(self.options['channel'], 'testchannel')
+
+	def testParseServer(self):
+		self.assertEqual(self.options['server'], 'testserver')
+
+	def testParsePort(self):
+		self.assertEqual(self.options['port'], '1234')
 
 if __name__ == '__main__':
 	unittest.main()
